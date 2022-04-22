2fb37b14b9a3152087a366986fe9c82ff7a91cf7
Grace <45552424+yyygrace@users.noreply.github.com>
2022-04-22 08:53:58 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,44 @@
-import static org.junit.Assert.*;
-import org.junit.*;
+import static org.junit.Assert.*;       // imports Assert
+import org.junit.*;                     // imports JUnit
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
-public class MarkdownParseTest {
+public class MarkdownParseTest {        // class header
+    @Test                               // tells Java that the following method will be a test
+    public void addition() {            // method header
+        assertEquals(2, 1 + 1);         // checks between the expected and actual output
+    }
+
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void testFileTester() throws IOException{
+        ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("test-file.md")));
+        ArrayList<String> exp = new ArrayList<String>();
+        exp.add("https://something.com");
+        exp.add("some-thing.html");
+        assertEquals(exp, links);
+    }
+
+    @Test
+    public void imageTester() throws IOException{
+        ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("image-tester.md")));
+        ArrayList<String> empty = new ArrayList<String>();
+        assertEquals(empty, links);
+    }
+
+    @Test
+    public void newTesterTester() throws IOException{
+        ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("new-tester.md")));
+        ArrayList<String> exp = new ArrayList<String>();
+        exp.add("www.google.com");
+        assertEquals(exp, links);
+    }
+
+    @Test
+    public void noLinksTester() throws IOException{
+        ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("no-links.md")));
+        ArrayList<String> empty = new ArrayList<String>();
+        assertEquals(empty, links);
     }
 }
