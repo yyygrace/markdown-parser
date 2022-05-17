5815a6e0e7f416efe20c9def3222ab06f7efb831
Grace <45552424+yyygrace@users.noreply.github.com>
2022-05-16 21:29:27 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -94,4 +94,34 @@ public class MarkdownParseTest {        // class header
         ArrayList<String> empty = new ArrayList<String>();
         assertEquals(empty, links);
     }
+
+    @Test
+    public void testSnipper1() throws IOException{
+        ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("snippet1.md")));
+        ArrayList<String> exp = new ArrayList<String>();
+        exp.add("`www.google.com");
+        exp.add("google.com");
+        exp.add("ucsd.edu");
+        assertEquals(exp, links);
+    }
+
+    @Test
+    public void testSnipper2() throws IOException{
+        ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("snippet2.md")));
+        ArrayList<String> exp = new ArrayList<String>();
+        exp.add("a.com");
+        exp.add("a.com(())");
+        exp.add("example.com");
+        assertEquals(exp, links);
+    }
+
+    @Test
+    public void testSnipper3() throws IOException{
+        ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("snippet3.md")));
+        ArrayList<String> exp = new ArrayList<String>();
+        exp.add("https://www.twitter.com");
+        exp.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+        exp.add("https://cse.ucsd.edu/");
+        assertEquals(exp, links);
+    }
 }
