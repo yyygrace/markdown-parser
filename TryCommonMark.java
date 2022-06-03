5e8dd5fffe0541715aa12b5d2654d8cd7ce1f2e9
Grace <45552424+yyygrace@users.noreply.github.com>
2022-06-03 08:38:07 -0700

    Update TryCommonMark.java

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -9,5 +9,27 @@ class TryCommonMark {
         HtmlRenderer renderer = HtmlRenderer.builder().build();
         renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
         System.out.println(renderer);
+
+        // this part actually does the computation
+    Node node = parser.parse("Example\n=======\n\nSome more text");
+    WordCountVisitor visitor = new WordCountVisitor();
+    node.accept(visitor);
+    System.out.println(visitor.wordCount);  // 4
+    }
+}
+
+// this class can be defined anywhere in the file
+class WordCountVisitor extends AbstractVisitor {
+    int wordCount = 0;
+
+    @Override
+    public void visit(Text text) {
+        // This is called for all Text nodes. Override other visit methods for other node types.
+
+        // Count words (this is just an example, don't actually do it this way for various reasons).
+        wordCount += text.getLiteral().split("\\W+").length;
+
+        // Descend into children (could be omitted in this case because Text nodes don't have children).
+        visitChildren(text);
     }
 }
